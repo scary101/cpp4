aad958a194c5a7e88d3d79117bc9036fff717377
scary101 <scary101@yandex.ru>
2024-02-20 22:57:22 +0300

    Добавьте файлы проекта.

---
+++ mass/mass.cpp
@@ -0,0 +1,30 @@
+﻿// mass.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	const int stroka = 5;
+	const int stolb = 3;
+	double arr[stolb][stroka];
+	for (int i = 0; i < stolb; i++) {
+		cout << "Stroka " << i + 1 << ": ";
+		for (int j = 0; j < stroka; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	for (int i = 0; i < stolb; i++) {
+		float sum = 0;
+		for (int j = 0; j < stroka; j++) {
+			sum += arr[i][j];
+		}
+		float avg = sum / stroka;
+		cout << "Среднее арифметическое элементов строки " << i + 1 << " = " << avg << endl;
+	}
+
+}
